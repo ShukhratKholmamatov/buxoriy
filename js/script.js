f5d323fcb4752e14eb0d10cd8055b48d865eff75
Shukhrat Kholmamatov <142324315+ShukhratKholmamatov@users.noreply.github.com>
2024-03-09 23:26:35 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,10 +19,16 @@ let counter = 0;
 
 function popOut() {
     let text = document.getElementById('tex');
+    let down = document.getElementById('down');
+    let up = document.getElementById('up');
     text.style.transition = ".3s";
     text.style.display = "block";
+    down.style.display = "none";
+    up.style.display = "block";
     counter++;
     if (counter%2 == 0) {
         text.style.display = "none";
+        down.style.display = "block";
+        up.style.display = "none";
     }
 }
